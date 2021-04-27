a1be0cd5afec2babdd899d417a7749c7963e4710
cicero.maciel@ufv.br <cicero.maciel@ufv.br>
2021-04-27 19:43:41 -0300

    Esqueci de trocar os nomes da classe equipe

--- equipe.h
+++ equipe.h
@@ -16,12 +16,14 @@
 
 class Equipe {
 public:
+    // VECTOR COM OS PERSONAGENS
     std::vector<Personagem*> _equipe;
-    Equipe(unsigned int personagem_1, std::string nome_personagem1, long double forca_base_personagem1, long double destreza_base_personagem1, long double constituicao_base_personagem1,
-           long double inteligencia_base_personagem1, unsigned int personagem_2, std::string nome_personagem2, long double forca_base_personagem2, long double destreza_base_personagem2,
-           long double constituicao_base_personagem2, long double inteligencia_base_personagem2, unsigned int personagem_3, std::string nome_personagem3, long double forca_base_personagem3,
-           long double destreza_base_personagem3, long double constituicao_base_personagem3, long double inteligencia_base_personagem3, unsigned int personagem_4, std::string nome_personagem4,
-           long double forca_base_personagem4, long double destreza_base_personagem4, long double constituicao_base_personagem4, long double inteligencia_base_personagem4);
+    // CONSTRUTOR
+    Equipe(unsigned int personagem_1,std::string nome_personagem1,long double forca_personagem1,long double destreza_personagem1,long double constituicao_personagem1,long double inteligencia_personagem1,
+           unsigned int personagem_2,std::string nome_personagem2,long double forca_personagem2,long double destreza_personagem2,long double constituicao_personagem2,long double inteligencia_personagem2,
+           unsigned int personagem_3,std::string nome_personagem3,long double forca_personagem3,long double destreza_personagem3,long double constituicao_personagem3,long double inteligencia_personagem3,
+           unsigned int personagem_4,std::string nome_personagem4,long double forca_personagem4,long double destreza_personagem4,long double constituicao_personagem4,long double inteligencia_personagem4);
+    // DESTRUTOR
     ~Equipe();
 };
 
